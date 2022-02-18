0ff0d8dea742deb6bbd44c49c728f8a526a6c402
imdaegeun <cd3818@naver.com>
2022-02-18 22:02:03 +0900

    2/18
    - spring boot와 연동
      - App.js에서 연동 테스트

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
-
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [ testStr, setTestStr] = useState('');
+
+  function callback(str) {
+    setTestStr(str);
+  }
+
+  useEffect(
+    () => {
+      axios (
+        {
+            url: '/api/v1/home'
+            , method: 'get'
+            , baseURL: 'http://localhost:8080'
+            , withCredentials: true
+        }
+    ).then((res) => {
+      callback(res.data);
+    });
+    }
+  );
+
   return (
     <div className="App">
-      Hello world!
+      {testStr}
     </div>
   );
 }
