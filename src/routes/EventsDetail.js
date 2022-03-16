ddd11aab6e3d3c14286a8bdc947bbd6b4f12d452
imdaegeun <cd3818@naver.com>
2022-03-16 20:45:30 +0900

    3/16
    - 모임 서브 이미지 여러개일경우 스크롤해서 보여주기
    - 모임 호스트일 경우, 서브 사진 추가 가능하도록 하기

--- src/routes/EventsDetail.js
+++ src/routes/EventsDetail.js
@@ -1,9 +1,11 @@
 import { ThemeProvider } from "@emotion/react";
-import { CardMedia, Container, createTheme, CssBaseline, Grid } from "@mui/material";
+import { Button, CardMedia, Container, createTheme, CssBaseline, Grid } from "@mui/material";
 import { Box } from "@mui/system";
 import React, { useEffect, useState } from "react"
 import { useParams } from "react-router-dom";
+import { LOCAL_STORAGE_CONST } from "../common/GlobalConst";
 import HEADER_SECTION from "../common/HeaderSection";
+import { RESPONSE_STATUS } from "../common/ResponseStatus";
 import { createMainImage } from "../common/Utils";
 import Header from "../component/Header";
 import MainFeaturedPost from "../component/MainFeaturedPost";
@@ -13,17 +15,36 @@ import API from "../config/customAxios";
 const EventsDetail = () => {
     const params = useParams();
     const [event, setEvent] = useState(null);
+    const [isHost, setIsHost] = useState(false);
     useEffect(() => {
         
         const getEvent = async (eventId) => {
             try {
                 const {data}  = await API.get(`/api/v1/events/${eventId}`);
-                if (data) {
-                    createMainImage(data);
-                    data.subImage = data.eventsImagesDtos.filter((post) => {
-                        return post.imagesType === 'MAIN';
+                const tmp_event = data;
+                if (tmp_event) {
+                    createMainImage(tmp_event);
+                    tmp_event.subImage = data.eventsImagesDtos.filter((post) => {
+                        return post.imagesType === '';
                     })
-                    setEvent(data);
+                    setEvent(tmp_event);
+
+                    // 내가 만든 모임일 경우 사진 추가 버튼 보이게.
+                    const jwt = localStorage.getItem(LOCAL_STORAGE_CONST.ACCESS_TOKEN);
+                    if (jwt) {
+                        const {data, status}  = await API.get(
+                                        `/api/v1/accounts`
+                                        , {
+                                            headers : {
+                                                'Authorization': jwt
+                                            }
+                                        });
+                        if (status === RESPONSE_STATUS.OK) {
+                            if (tmp_event.hostAccountsId === data.id) {
+                                setIsHost(true);
+                            }
+                        }
+                    }
                 }
             } catch(e) {
                 console.log(e);
@@ -46,18 +67,36 @@ const EventsDetail = () => {
                         (   
                             <Container component="main" >
                                 <MainFeaturedPost post={event} />
-                                {/* <Grid container spacing={4}>
-                                </Grid> */}
-                                <Box style={{maxHeight: '100vh', overflow: 'auto'}} overflow-x>
+                                <Box
+                                    mb={2}
+                                    display="flex"
+                                    flexDirection="row"
+                                    style={{
+                                        maxHeight: '100vh', // fixed the height
+                                        overflow: "scroll",
+                                        overflowY: "hidden" // added scroll
+                                    }}
+                                >
                                     {event.subImage.map(subImage => (
                                         <CardMedia
                                             component="img"
-                                            sx={{ width: 160, display: { xs: 'inline' } }}
+                                            sx={{ width: 160, m: 1, display: { xs: 'inline' } }}
                                             image={subImage.image}
+                                            style={{
+                                                borderRadius: 2
+                                            }}
                                         />
                                         ))
                                     }
-                                   
+                                    {
+                                        isHost && 
+                                        (
+                                            <Button sx={{ width: 160, m:1, border: '1px dashed grey' }} >
+                                                사진 추가
+                                            </Button>
+                                        )
+                                    }
+                                    
                                 </Box>
                             </Container>
                         )
