54c776929f5d37478a63b19cdd34c51908b75254
imdaegeun <cd3818@naver.com>
2022-04-14 20:52:27 +0900

    4/14
    - 페이징 리스트 코드 리팩터링

--- src/component/PaginatedItems.js
+++ src/component/PaginatedItems.js
@@ -1,10 +1,6 @@
 import { Grid, Pagination } from '@mui/material';
 import { Box } from '@mui/system';
-import moment from 'moment';
-import React, { useEffect, useState } from 'react';
-import { RESPONSE_STATUS } from '../common/ResponseStatus';
-import { createEventMainImage } from '../common/Utils';
-import API from '../config/customAxios';
+import React from 'react';
 import FeaturedPost from './FeaturedPost';
 
 const Items = ({ currentItems }) => {
@@ -17,60 +13,7 @@ const Items = ({ currentItems }) => {
   );
 }
 
-const PaginatedItems = ({ itemsPerPage }) => {
-    // We start with an empty list of items.
-    const [currentItems, setCurrentItems] = useState([]);
-    const [totalItems, setTotalItems] = useState(0);
-    const [pageCount, setPageCount] = useState(0);
-    // Here we use item offsets; we could also use page offsets
-    // following the API or data you're working with.
-
-    useEffect(
-        () => {
-            getEventsList(0);
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-        } ,[itemsPerPage]
-    );
-
-    const getEventsList = async (page) => {
-        try {
-            const {data, status} = await API.get(`/api/v1/events?page=${page}`);
-            const tmp_events = data;
-            if (status === RESPONSE_STATUS.OK) {
-                printMainPage(tmp_events);
-                if (totalItems === 0) {
-                    await getEventsTotal();
-                }
-            }
-        } catch(e) {
-            console.log(e);
-        }
-    }
-
-    const printMainPage = (data) => {
-      data.map(event => 
-          {
-              event.key = data.id;
-              createEventMainImage(event);
-              event.date = moment(event.eventsTime).format('YYYY-MM-DD HH시 mm분');
-              return event;
-          }
-      )
-      setCurrentItems(data);
-    }
-
-    const getEventsTotal = async () => {
-        const {data, status} = await API.get("/api/v1/events/size");
-        if (status === RESPONSE_STATUS.OK) {
-          setTotalItems(data);
-          setPageCount(Math.ceil(data / itemsPerPage));
-        }
-    }
-
-  // Invoke when user click to request another page.
-  const handlePageClick = async (event, value) => {
-    getEventsList(value - 1);
-  };
+const PaginatedItems = ({ handlePageClick, currentItems, pageCount }) => {
 
   return (
     <>
--- src/routes/EventsList.js
+++ src/routes/EventsList.js
@@ -1,11 +1,68 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import HEADER_SECTION from "../common/HeaderSection";
 import { Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
 import Header from "../component/Header";
 import PaginatedItems from "../component/PaginatedItems";
+import API from "../config/customAxios";
+import { RESPONSE_STATUS } from "../common/ResponseStatus";
+import { createEventMainImage } from "../common/Utils";
+import moment from "moment";
 
 
 const EventsList = () => {
+
+    // We start with an empty list of items.
+    const itemsPerPage = 9;
+    const [currentItems, setCurrentItems] = useState([]);
+    const [totalItems, setTotalItems] = useState(0);
+    const [pageCount, setPageCount] = useState(0);
+
+    useEffect(
+        () => {
+            getEventsList(0);
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+        } ,[itemsPerPage]
+    );
+
+    const getEventsList = async (page) => {
+        try {
+            const {data, status} = await API.get(`/api/v1/events?page=${page}`);
+            const tmp_events = data;
+            if (status === RESPONSE_STATUS.OK) {
+                printMainPage(tmp_events);
+                if (totalItems === 0) {
+                    await getEventsTotal();
+                }
+            }
+        } catch(e) {
+            console.log(e);
+        }
+    }
+
+    const printMainPage = (data) => {
+      data.map(event => 
+          {
+              event.key = data.id;
+              createEventMainImage(event);
+              event.date = moment(event.eventsTime).format('YYYY-MM-DD HH시 mm분');
+              return event;
+          }
+      )
+      setCurrentItems(data);
+    }
+
+    const getEventsTotal = async () => {
+        const {data, status} = await API.get("/api/v1/events/size");
+        if (status === RESPONSE_STATUS.OK) {
+          setTotalItems(data);
+          setPageCount(Math.ceil(data / itemsPerPage));
+        }
+    }
+
+      // Invoke when user click to request another page.
+    const handlePageClick = async (event, value) => {
+        getEventsList(value - 1);
+    };
 
     
     const theme = createTheme();
@@ -16,7 +73,7 @@ const EventsList = () => {
             <Container maxWidth="lg">
                 <Header title={HEADER_SECTION.title} sections={HEADER_SECTION.sections} />
                 <main>
-                    <PaginatedItems itemsPerPage={8} />
+                    <PaginatedItems handlePageClick={handlePageClick} currentItems={currentItems} pageCount={pageCount} />
                 </main>
             </Container>
         </ThemeProvider>
